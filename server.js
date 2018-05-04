e741ac02ee7845c126c6c022ba2f96fc014ef865
bharding2 <b.harding@gmail.com>
2018-05-04 15:21:17 -0700

    updated server with library routing

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-if (!process.env.APP_SECRET) process.env.APP_SECRET = 'secret';
-
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5555;
 
-const connection =  mongoose.createConnection(process.env.MONGODB_URI ||
-  'mongodb://localhost/slothbear_books');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/slothbear_books_DB');
+
+const libraryRouter = require('./routes/library_router');
+
+app.use('/api', libraryRouter);
 
 module.exports = app.listen(PORT, () => console.log(`server up on port ${PORT}`));
