b9c8fef427590667f46946a3c221a9ffded1c706
bharding2 <b.harding@gmail.com>
2018-05-14 15:24:15 -0700

    hooking up book routes

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 5555;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/slothbear_books_DB');
 
 const libraryRouter = require('./routes/library_router');
+const bookRouter = require('./routes/book_router');
 
 app.use('/api', libraryRouter);
+app.use('/api', bookRouter);
 
 module.exports = app.listen(PORT, () => console.log(`server up on port ${PORT}`));
